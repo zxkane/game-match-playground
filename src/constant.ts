72330737c46ddb57e0c2f026d903b2a600a41fc9
Mengxin Zhu <843303+zxkane@users.noreply.github.com>
2024-12-04 16:38:55 +0800

    chore: update the env name

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'FC Game Playground';
 
-export const isGoogleAuthEnabled = process.env.NEXT_PUBLIC_EXTERNAL_PROVIDERS?.includes('google') || false;
-export const oidcProvider = process.env.NEXT_PUBLIC_OIDC_PROVIDER;
+export const isGoogleAuthEnabled = process.env.NEXT_PUBLIC_AUTH_EXTERNAL_PROVIDERS?.includes('google') || false;
+export const oidcProvider = process.env.NEXT_PUBLIC_AUTH_OIDC_PROVIDER;
