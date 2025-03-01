e01152a0b28a06890ad228a6c69ec7b93b0a3af1
Mengxin Zhu <843303+zxkane@users.noreply.github.com>
2025-03-01 21:07:39 +0800

    chore: bump dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,30 @@ module.exports = {
   overrides: [
     {
       files: ['amplify/data/**/*.js'],
-      extends: 'plugin:@aws-appsync/base',
       plugins: ['@aws-appsync'],
       globals: {
         util: 'readonly',
         context: 'readonly'
       },
+      parserOptions: {
+        ecmaVersion: 2018
+      },
+      rules: {
+        '@aws-appsync/no-async': 'error',
+        '@aws-appsync/no-await': 'error',
+        '@aws-appsync/no-classes': 'error',
+        '@aws-appsync/no-for': 'error',
+        '@aws-appsync/no-continue': 'error',
+        '@aws-appsync/no-generators': 'error',
+        '@aws-appsync/no-yield': 'error',
+        '@aws-appsync/no-labels': 'error',
+        '@aws-appsync/no-this': 'error',
+        '@aws-appsync/no-try': 'error',
+        '@aws-appsync/no-while': 'error',
+        '@aws-appsync/no-disallowed-unary-operators': 'error',
+        '@aws-appsync/no-disallowed-binary-operators': 'error',
+        '@aws-appsync/no-promise': 'error'
+      }
     }
   ]
 }
